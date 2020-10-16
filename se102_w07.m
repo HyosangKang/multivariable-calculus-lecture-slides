6414e94be45137475e78f40b23251e40eeebc322
Hyosang Kang <hyosang@dgist.ac.kr>
2020-10-16 11:10:56 +0900

    Update se102_w07.m

--- se102_w07.m
+++ se102_w07.m
@@ -5,7 +5,7 @@ fy = matlabFunction(diff(f,y), 'vars', {x,y});
 
 f = matlabFunction(f, 'vars', {x,y});
 F = @(t,c) [-fx(c(1),c(2)); -fy(c(1),c(2))];
-[~, c] = ode45(F, linspace(0, 10), [1, 1]);
+[~, c] = ode45(F, linspace(0, 10), [1, 0]);
 
 [X, Y] = meshgrid(linspace(-6,6));
 surf(X,Y,f(X,Y));
@@ -17,10 +17,27 @@ hold off;
 axis equal;
 
 %%
-a = 7 ; b = 3 ; 
-x = @(t) (a-b)*cos(b/a*t) + b*cos((1-b/a)*t) ;
-y = @(t) (a-b)*sin(b/a*t) - b*sin((1-b/a)*t) ;
-t = linspace(0,2*a*pi) ;
-plot(x(t),y(t)) ; 
-axis equal ; axis([-a,a,-a,a]) ;
+a = 7 ; b = 5 ; 
+x = @(t) (a-b)*cos(t) + b*cos((b/a)*t) ;
+y = @(t) (a-b)*sin(t) - b*sin((b/a)*t) ;
+t = linspace(0,2*a*pi,1000) ;
+theta = linspace(0,2*pi) ;
+for i = 1 : 1000
+    plot(x(t(1:i)),y(t(1:i))) ; hold on;
+    plot(a*cos(theta), a*sin(theta));
+    plot(b*cos(theta) + (a-b)*cos(t(i)), ...
+        b*sin(theta) + (a-b)*sin(t(i)));
+    plot(x(t(i)),y(t(i)), '.r', 'markersize', 20) ;
+    hold off;
+    axis equal ; axis([-a,a,-a,a]) ;
+    drawnow;
+end
+
+%%
+syms x y z l
+g = x^2 + y^2 + z^2;
+f = x*y^2*z^3;
+eqn = [l*gradient(f) == gradient(g); f==2];
+S = solve(eqn, [x,y,z,l]);
+S.x
     
